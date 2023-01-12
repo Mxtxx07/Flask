d5342230788c0c98f14caff1226ef65c460253d0
Mxtxx07 <119619265+Mxtxx07@users.noreply.github.com>
2023-01-12 00:41:30 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,10 @@
+from conexion_postgresql import abrirconexion
+
+def registro():
+        conexion = abrirconexion()
+        cursor   = conexion.cursor()
+        cursor.execute('SELECT * FROM usuario')
+        print(cursor.fetchall())
+        conexion.commit()
+        
+registro()
